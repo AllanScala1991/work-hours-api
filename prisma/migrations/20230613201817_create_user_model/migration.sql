a57c4ab5a35b7f0973c72667a4b2c5c83aed0c79
Allan Scala <allanscala@gmail.com>
2023-06-13 17:20:25 -0300

    feat: create user prisma model

---
+++ prisma/migrations/20230613201817_create_user_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "position" TEXT NOT NULL,
+    "sector" TEXT NOT NULL,
+    "authorization" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
