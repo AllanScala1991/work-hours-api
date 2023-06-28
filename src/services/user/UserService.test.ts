9f499a79898be9c18564daa992ebc5661ea836ca
Allan Scala <allanscala@gmail.com>
2023-06-28 15:47:22 -0300

    feat: create user service unit test

--- src/services/user/UserService.test.ts
+++ src/services/user/UserService.test.ts
@@ -26,4 +26,38 @@ describe("User Service Tests", () => {
         expect(user.length).toBeGreaterThan(0);
         expect(user[0].name).toEqual("Test");
     })
+
+    test("Create new user", async () => {
+        jest.spyOn(prisma.users, 'create').mockImplementationOnce((): any => {
+            return {
+                id: "123",
+                name: "Test",
+                email: "test@mail.com",
+                phone: "41999999999",
+                address: "test",
+                username: "test",
+                password: "123",
+                position: "test",
+                sector: "test",
+                authorization: "test",
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        })
+
+        const user = await userService.createUser({
+            id: "123",
+            name: "Test",
+            email: "test@mail.com",
+            phone: "41999999999",
+            address: "test",
+            username: "test",
+            password: "123",
+            position: "test",
+            sector: "test",
+            authorization: "test",
+        });
+
+        expect(user.name).toEqual("Test");
+    })
 })
