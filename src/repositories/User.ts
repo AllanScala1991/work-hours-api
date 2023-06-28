2be150eeac1c5e1925e8563b3cfb60dc0d7c5960
Allan Scala <allanscala@gmail.com>
2023-06-28 15:44:47 -0300

    feat: insert create user in user model

--- src/repositories/User.ts
+++ src/repositories/User.ts
@@ -2,4 +2,5 @@ import { UserModel } from "../models/User";
 
 export interface UserRepository {
     findUserByUsername(username: string): Promise<UserModel[]>
+    createUser(user: UserModel): Promise<UserModel>
 }
