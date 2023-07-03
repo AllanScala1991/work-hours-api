c37f0fd8bc762f1224be1caf47d8be27dcd1e549
Allan Scala <allanscala@gmail.com>
2023-07-03 19:49:06 -0300

    refactor: remove auth in create user route

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,12 +1,11 @@
 import { Request, Response, Router } from "express";
 import { UserController } from "../controllers/user/UserController";
-import auth from "../middlewares/Authentication";
 import apiToken from "../middlewares/CreateUserAuth";
 
 
 const app: Router = Router();
 const userController: UserController = new UserController();
 
-app.post("/user/create", auth, apiToken, async (req: Request, res: Response) => await userController.create(req, res));
+app.post("/user/create", apiToken, async (req: Request, res: Response) => await userController.create(req, res));
 
 module.exports = app;
