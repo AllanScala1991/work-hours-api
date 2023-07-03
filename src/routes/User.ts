8f5bd70e6047798431a9186e19f96eb9b0dffadd
Allan Scala <allanscala@gmail.com>
2023-07-03 19:47:51 -0300

    feat: insert validator api token create user in routes

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from "express";
 import { UserController } from "../controllers/user/UserController";
+import auth from "../middlewares/Authentication";
+import apiToken from "../middlewares/CreateUserAuth";
 
 
 const app: Router = Router();
 const userController: UserController = new UserController();
 
-app.post("/user/create", async (req: Request, res: Response) => await userController.create(req, res));
+app.post("/user/create", auth, apiToken, async (req: Request, res: Response) => await userController.create(req, res));
 
 module.exports = app;
