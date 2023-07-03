a88a18d0cf2c0845f09a729f7cfaf1ddaa6590f7
Allan Scala <allanscala@gmail.com>
2023-07-03 19:33:21 -0300

    refactor: user controller insert encrypterRepository parameter

--- src/controllers/user/UserController.ts
+++ src/controllers/user/UserController.ts
@@ -1,15 +1,22 @@
 import { Request, Response } from "express";
 import { CreateUserModel } from "../../models/User";
 import { createNewUser } from "../../usecases/user/CreateNewUser";
+import { EncrypterRepository } from "../../repositories/Encrypter";
+import { Bcrypt } from "../../helpers/bcrypt/Bcrypt";
 
 
 export class UserController {
+    private encrypterRepository: EncrypterRepository;
+
+    constructor(){
+        this.encrypterRepository = new Bcrypt();
+    }
     
     async create(req: Request, res: Response) {
         try {
             const  user: CreateUserModel = req.body;
             
-            const response = await createNewUser(user);
+            const response = await createNewUser(user, this.encrypterRepository);
 
             return res.status(201).json(response);
             
